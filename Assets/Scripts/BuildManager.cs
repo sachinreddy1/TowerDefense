d673766476ac5ca65e5190141605b4cf397b7765
sachinreddy1 <sachinreddy1009@gmail.com>
2019-06-01 01:03:23 -0400

    Building and camera controls.

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+    
+    void Awake() {
+        if (instance != null)
+        {
+            Debug.Log("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+    public GameObject startTurretPrefab;
+
+    void Start() {
+        turretToBuild = startTurretPrefab;
+    }
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild() {
+        return turretToBuild;
+    }
+}
