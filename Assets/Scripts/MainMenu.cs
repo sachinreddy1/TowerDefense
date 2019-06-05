dd4e6a9d4a5982050b8d540f9a939e01cbafbfbe
sachinreddy1 <sachinreddy1009@gmail.com>
2019-06-05 01:36:33 -0400

    menu things

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public string levelToload = "MainScene";
+    public void Play() {
+        SceneManager.LoadScene(levelToload);
+    }
+
+    public void Quit() {
+        Debug.Log("Quit.");
+        Application.Quit();
+    }
+}
